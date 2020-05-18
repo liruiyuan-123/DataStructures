8b46a041b84e27c86526fc5e5ab323c3efe64edf
lry <lry@example.com>
2020-05-18 19:05:20 +0800

    树

--- DS5_SortAndSearch/3.sort.py
+++ DS5_SortAndSearch/3.sort.py
@@ -1,4 +1,34 @@
+# def bubbleSort(alist):
+#     for passnum in range(len(nums)-1,0,-1):
+#         for i in range(passnum):
+#             if nums[i] > nums[i+1]:
+#                 temp = nums[i]
+#                 nums[i] = nums[i+1]
+#                 nums[i+1] = temp
+# nums = [5,2,3,1]
+# bubbleSort(nums)
+# print(nums)
+
+# 选择排序
+# def seletcSort(alist):
+#     for fillslot in range(len(nums)-1,0,-1):
+#         positionMax = 0
+#         for location in range(1,fillslot+1):
+#             if nums[location] > nums[positionMax]:
+#                 positionMax = location
+#         temp = nums[fillslot]
+#         nums[fillslot] = nums[positionMax]
+#         nums[positionMax] = temp
+
+# nums = [5,2,3,1,0]
+# seletcSort(nums)
+# print(nums)
+
+
 '''
+    LeetCode第912题 排序数组
+    给一个整数数组nums,将数组升序排列
+    nums = [5,2,3,1]
     [54,26,93,17,77,31,44,55,20]
     一、冒泡排序:对一个列表多次遍历，比较相邻的两项，并且交换顺序排错的项。
         每对列表进行一次遍历，就有一个最大项排在了正确的位置，大体上讲，
@@ -50,11 +80,10 @@ print(alist)
 '''
 
 
-
-
 '''
     二、选择排序：每遍历一次列表只交换一次数据，也就是进行一次遍历时找到最大的项
         完成遍历后，再把它换到正确的位置
+        每次遍历都是找最大值，放在后面
            alist = [26,54,93,17,77,31,44,55,20]
         第1次遍历   [26,54,20,17,77,31,44,55,93]   93
         第2次遍历   [26,54,20,17,55,31,44,77,93]   77
@@ -64,7 +93,6 @@ print(alist)
         第6次遍历   [26,31,20,17,44,54,55,77,93]   31
         第7次遍历   [20,17,26,31,44,54,55,77,93]   26
         第8次遍历   [17,20,26,31,44,54,55,77,93]   20
-'''
 def seletcSort(alist):
     for fillslot in range(len(alist)-1,0,-1):
         positionMax = 0
@@ -72,11 +100,201 @@ def seletcSort(alist):
         for location in range(1,fillslot+1):
             if alist[location] > alist[positionMax]:
                 positionMax = location
-
         temp = alist[fillslot]
         alist[fillslot] = alist[positionMax]
         alist[positionMax] = temp
-
 alist = [26,54,93,17,77,31,44,55,20]
 seletcSort(alist)
+print(alist)
+'''
+
+
+'''
+    三、插入排序 ：总是保持一个位置靠前的已经排好的字表 [54] [26,93,17,77,31,44,55,20],
+        然后每一个新的数据项被“插入”到前面的子表中，排好的第一个字表就增加了一项
+        alist = [54,26,93,17,77,31,44,55,20]
+        分成两个字表  [54] [26,93,17,77,31,44,55,20]
+        第1步    [26,54]   [93,17,77,31,44,55,20]  插入26
+        第2步    [26,54,93]   [17,77,31,44,55,20]  插入93
+        第3步    [17,26,54,93]   [77,31,44,55,20]  插入17
+        第4步    [17,26,54,77,93]   [31,44,55,20]  插入77
+        第5步    [17,26,31,54,77,93]   [44,55,20]  插入31
+        第6步    [17,26,31,44,54,77,93]   [55,20]  插入44
+        第7步    [17,26,31,44,54,55,77,93]  [20]   插入55
+        第8步    [17,20,26,31,44,54,55,77,93] []   插入20
+def insertSort(alist):
+    for index in range(1,len(alist)):
+        currentValue = alist[index]
+        position = index
+        while position > 0 and alist[position-1] > currentValue:
+            alist[position] = alist[position - 1]
+            position = position - 1
+            alist[position] = currentValue
+alist = [54,26,93,17,77,31,44,55,20]
+insertSort(alist)
+print(alist)
+'''
+
+
+'''
+    四、希尔排序(缩小间隔排序):以插入排序为基础，将原来要排序的列表划分成一些字列表，
+        再对每一个子列表执行插入操作，从而实现对插入排序性能的改进。
+        关键：划分子列表的特定方法
+        alist = [54,26,93,17,77,31,44,55,20]
+    
+        1. 以3为间隔来划分，分成3个字列表
+           [54,17,44]  [17,26,93,44,77,31,54,55,20]
+           [26,77,55]  [17,26,93,44,55,31,54,77,20]
+           [93,31,20]  [17,26,20,44,55,31,54,77,93]
+           以1为间隔，也就是进行标准插入排序：[17] [26,20,44,55,31,54,77,93]
+           优化了比对和移动的次数
+        2. 以2为间隔来划分，分成5个字列表
+# LeetCode第147题 对链表进行排序
+def shellSort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+        for i in range(sublistcount):
+            gapInsertionSort(alist,i,sublistcount)
+    
+        print("当前子列表为:",sublistcount,"当前整体列表为：",alist)
+        sublistcount = sublistcount//2
+# 特定间隔分子列表，然后执行标准插入排序
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+        currentValue = alist[i]
+        position = i
+        while position >= gap and alist[position-gap] > currentValue:
+            alist[position] = alist[position-gap]
+            position = position - gap
+        alist[position] =  currentValue
+alist = [54,26,93,17,77,31,44,55,20]
+shellSort(alist)
+print(alist)
+'''
+
+
+'''
+    五、归并排序（mergeSort）
+        具体做法：递归算法，不断的将列表拆分为一半。
+        如果列表为空或者只有一个元素，那么根据定义，它就被排好了
+        alist = [54,26,93,17,77,31,44,55,20]
+        第1次拆分：   [54,26,93,17]              [77,31,44,55,20]
+        第2次拆分： [54,26]   [93,17]           [77,31]     [44,55,20]
+        第3次拆分：[54] [26] [93]  [17]        [77]  [31]  [44]  [55,20]
+        第4次拆分                                               [55] [20]
+归并过程
+        [54]   [26]     [93]  [17]    [77]   [31]   [44] [55]   [20]
+1          [26,54]         [17,93]       [31,77]            [20,55]
+2              [17,26, 54,93]                          [20,44,55]
+3                                             [20,31,44,55,77]
+4                       [17,20,26,31,44,54,55,77,93]
+'''
+
+
+# def mergeSort(alist):
+#     if len(alist) > 1:
+#         mid = len(alist)//2
+#         lefthalf = alist[:mid]
+#         righthalf = alist[mid:]
+
+#         mergeSort(lefthalf)
+#         mergeSort(righthalf)
+
+#         i = 0
+#         j = 0
+#         k = 0
+
+#         while i < len(lefthalf) and j < len(righthalf):
+#             if lefthalf[i] < righthalf[j]:
+#                 alist[k] = lefthalf[i]
+#                 i = i+1
+#             else:
+#                 alist[k] = righthalf[j]
+#                 j = j+1
+#             k = k+1
+
+#         while i < len(lefthalf):
+#             alist[k] = lefthalf[i]
+#             i = i+1
+#             k = k+1
+        
+#         while j < len(righthalf):
+#             alist[k] = righthalf[j]
+#             j = j+1
+#             k = k+1
+
+#         print("归并：",alist)
+
+
+# alist = [54,26,93,17,77,31,44,55,20]
+# mergeSort(alist)
+# print(alist)
+
+
+'''
+    六、快速排序
+        选择一个值作为枢纽值，一般就是列表第一项，枢纽值作为帮助拆分的标准，
+        alist = [54,26,93,17,77,31,44,55,20]
+                [17,20,26,31,44,54,55,77,93]
+        54最终在原列表中31的位置上，这个位置叫做：拆分点
+        54     左标记 [26,93,17,77,31,44,55,20] 右标记  
+        首先增加左标记，知道找到一个大于枢纽值得值，停止，然后递减右标记，知道找到一个小于枢纽值的值，停止。
+        交换这两项
+        增加左标记
+        26 < 54
+        93 > 54  stop
+        递减右标记
+        20 < 54  stop
+        交换
+        第一次的结果  [54,26,20,17,77,31,44,55,93]
+        第二次的结果  [54,26,20,17,44,31,77,55,93]
+        第三次的结果  [31,26,20,17,44,54,77,55,93]
+        根据拆分点，拆左右两个列表
+        [31,26,20,17,44]   [77,55,93]
+        重复以上过程
+        [17,26,20,31,44]   [55,77,93]
+        [17,26,20]
+        [26,20]
+        [20,26]
+'''
+def quickSort(alist):
+    quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+    if first < last:
+        splitpoint = partition(alist,first,last)
+        quickSortHelper(alist,first,splitpoint-1)
+        quickSortHelper(alist,splitpoint+1,last)
+
+def partition(alist,first,last):
+    pivotvalue = alist[first]
+
+    leftmark  = first + 1
+    rightmark = last
+
+    done = False
+    while not done:
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+        
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+    # 找到正确的位置,与枢纽值做交换
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+    return rightmark
+        
+
+
+alist = [54,26,93,17,77,31,44,55,20]
+quickSort(alist)
 print(alist)
